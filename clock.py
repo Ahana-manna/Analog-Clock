9a2c246419b69a896b4604be031bdb7cf79a2b4f
Ahana-manna <113796803+Ahana-manna@users.noreply.github.com>
2023-03-09 00:40:16 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,83 +1,113 @@
 import turtle
+from datetime import datetime
 
-t=turtle.Turtle()
-t.pensize(3)
+t = turtle.Turtle()
+t.pensize(7)
 t.color("green")
 t.screen.bgcolor("black")
-t.hideturtle()
 
-x=0
-y=-140
+t.hideturtle()
+x = 0
+y = -140
 t.penup()
-t.goto(x,y)
+t.goto(x, y)
 t.pendown()
 t.circle(140)
 t.penup()
 t.home()
-y=-100
-t.goto(x,y)
+y = -100
+t.goto(x, y)
 t.pendown()
+t.pensize(5)
 t.color("blue")
 t.circle(100)
 t.penup()
 t.home()
 t.left(60)
 
-def clock(h,m,s,t):
-    for i in range(12):
-        t.forward(120)
-        t.right(30)
-        t.color("yellow")
-        t.write((i + 1))
-        t.goto(0,0)
-    t.pendown()
 
-    #hour
+def fun_hr(h):
     t.penup()
     t.home()
     t.setheading(90)
-    angle=(h/12)*360
+    angle = (h / 12) * 360
     t.right(angle)
-    t.color("red")
-    t.pensize(3)
+    t.pensize(4)
     t.pendown()
     t.forward(60)
 
-    # minute
+
+def fun_min(m):
     t.penup()
     t.home()
     t.setheading(90)
     angle = (m / 60) * 360
     t.right(angle)
-    t.color("gold")
-    t.pensize(2)
+    t.pensize(3)
     t.pendown()
     t.forward(80)
+    t.penup()
+    t.goto(0, 0)
+
+
+def clock(h, m, s, t):
+    for i in range(12):
+        t.forward(120)
+        t.right(30)
+        t.color("yellow")
+        t.write((i + 1))
+        t.goto(0, 0)
+    t.pendown()
+
+    # hour
+    t.color("red")
+    fun_hr(h)
+
+    # minute
+    t.color("gold")
+    fun_min(m)
 
     # second
     a = 1
-    an: int= 0
-    c: int=1
+
     while a > 0:
-        if an == 0 or an > 0:
-            t.penup()
-            t.setheading(90)
-            angle = (s / 60) * 360
-            t.right(angle+an)
-            t.color("green")
-            t.pensize(1)
-            t.pendown()
-            t.forward(73)
-            t.undo()
-            t.goto(0,0)
-            an+=6
+        t.speed(6)
+        t.setheading(90)
+        angle = (s / 60) * 360
+        t.right(angle)
+        t.color("green")
+        t.pensize(2)
+        t.forward(73)
+        t.pendown()
+        t.undo()
+        t.color("black")
+        t.penup()
+        t.goto(0, 0)
+        s += 1
 
-            c=s+c
-            if c%60==0:
-                m=m+1
+        if s % 60 == 0:
+            m1=m+1
+            t.color("black")
+            fun_min(m)
+            t.color("gold")
+            fun_min(m1)
+            m=m1
 
-                if m%60==0:
-                    h=h+1
+            if m1 % 60 == 0:
+                h1 = h + 1
+                t.color("black")
+                fun_hr(h)
+                t.color("red")
+                fun_hr(h1)
+                h=h1
+        t.pendown()
 
-clock(10,55,26,t)
+
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+second = now.second
+
+clock(hour, minute, second, t)
+
 turtle.done()
